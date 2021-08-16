041b00664f89cd3a1fe6d749c28707c5bec6c7b7
高志鹏 <imbagaozp@163.com>
2021-08-16 14:27:11 +0800

    up log

--- baselib/src/main/java/com/aligit/base/ext/tool/LogExt.kt
+++ baselib/src/main/java/com/aligit/base/ext/tool/LogExt.kt
@@ -4,12 +4,14 @@ package com.aligit.base.ext.tool
 import com.elvishew.xlog.XLog
 
 fun _log(s:Any, t:Int=0){
-    when(t){
-        1 -> XLog.i(s)
-        else -> XLog.e(s)
-    }
+    try {
+        when (t) {
+            1 -> XLog.i(s)
+            else -> XLog.e(s)
+        }
+    }catch (e:Exception){}
 }
 
-fun log(s:Any){
-    _log(s)
-}
+fun logi(s:Any) = _log(s,1)
+
+fun log(s:Any) = _log(s)
