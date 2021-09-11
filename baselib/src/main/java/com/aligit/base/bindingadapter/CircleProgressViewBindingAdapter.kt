35e065672a41f14592c93df38b2b028c06c64e27
高志鹏 <imbagaozp@163.com>
2021-09-11 15:40:22 +0800

    update

--- baselib/src/main/java/com/aligit/base/bindingadapter/CircleProgressViewBindingAdapter.kt
+++ baselib/src/main/java/com/aligit/base/bindingadapter/CircleProgressViewBindingAdapter.kt
@@ -14,7 +14,7 @@ import com.xuexiang.xui.widget.progress.CircleProgressView
  * gStartColor 进度起始颜色(优先)
  * gEndColor 进度结束颜色
  * gTrackColor 进度条的底色（默认取进度颜色的透明 0.1）
- * gDuration 动画执行时间
+ * gDuration 动画执行时间 单位 毫秒，默认 1000
  */
 @BindingAdapter(
     value = ["gProgress", "gStartProgress", "gProgressColor", "gStartColor", "gEndColor", "gTrackColor","gDuration"],
@@ -28,10 +28,11 @@ fun setData(
     startColor: Int? = null,
     endColor: Int? = null,
     trackColor: Int? = null,
-    duration: Int = 1000
+    duration: Int? = null
 ) {
     view.run {
         dowithTry{
+            setProgressDuration(duration ?: 1000)
             (startColor ?: progressColor)?.let { it1 -> setStartColor(it1) }
             (endColor ?: progressColor)?.let { it1 -> setEndColor(it1) }
             trackColor?.let {
@@ -39,7 +40,6 @@ fun setData(
             } ?: (startColor ?: progressColor)?.let { it1 -> setTrackColor(ColorUtils.setColorAlpha(it1, 0.1f)) }
             setStartProgress((startProgress ?: 0).toFloat())
             setEndProgress(gress.toFloat())
-            setProgressDuration(duration)
             startProgressAnimation()
         }
     }
