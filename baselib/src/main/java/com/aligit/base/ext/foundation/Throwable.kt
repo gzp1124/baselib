6ae3be32417df0c1824936891b415fdcaf055d58
高志鹏 <imbagaozp@163.com>
2021-09-14 09:31:04 +0800

    update

--- baselib/src/main/java/com/aligit/base/ext/foundation/Throwable.kt
+++ baselib/src/main/java/com/aligit/base/ext/foundation/Throwable.kt
@@ -17,14 +17,17 @@ sealed class BaseThrowable(
 ) :
     Throwable(message, cause) {
 
+    // 外部错误，一般是指非业务的错误，比如 空指针，json 转换异常，网络请求超时 等
     class ExternalThrowable(throwable: Throwable) :
         BaseThrowable(cause = throwable)
 
+    // 内部错误，业务相关的异常，比如 账号无权访问，用户数据禁止修改 等等
     class InsideThrowable(
         val errorCode: String,
         val errorMessage: String
     ) : BaseThrowable(code = errorCode, message = errorMessage)
 
+    // token 错误，是内部错误的一种，一般指 账号被顶，Token失效
     class TokenThrowable : BaseThrowable()
 
     fun isExternal() = this is ExternalThrowable
